961b7922f2c0e0f3ed1e7a8772b8e36413af4c60
Nuri Salih Ersoy <119161232+Nurio34@users.noreply.github.com>
2023-09-20 18:29:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-// error handler
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
 
 app.use(express.static("./project"))
 app.use(express.json());
-// extra packages
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.set("trust proxy",1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+}))
 
 const AuthRouter = require("./routes/auth")
   app.use("/api/v1/auth",AuthRouter)
